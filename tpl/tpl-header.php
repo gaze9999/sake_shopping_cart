a54fc38c3eebdee19bda96f83a8f1d3dd1d85bc9
gaze9999 <gaze9999@livemail.tw>
2019-12-22 03:04:55 +0800

    add cart and login

--- tpl/tpl-header.php
+++ tpl/tpl-header.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="./src/sass/nav.css">
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <div class="nav_logo">
+<nav class="navbar navbar-expand-lg fixed-top">
+    <div class="nav_logo d-flex center-all">
         <header>
             <a class="navbar-brand d-flex" href="./index.php">
                 <h1>本格の酒</h1>
@@ -16,20 +16,21 @@
     <div id="my-nav" class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">地圖</a>
+                <a class="nav-link" href="">地圖</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">商品列表</a>
+                <a class="nav-link" href="">商品列表</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">清酒知識+</a>
+                <a class="nav-link" href="">清酒知識+</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">關於我們</a>
+                <a class="nav-link" href="">關於我們</a>
             </li>
         </ul>
     </div>
     <div class="nav_function">
-        <button></button>
+        <button class="btn btn-light">購物車</button>
+        <button class="btn btn-light">登入</button>
     </div>
 </nav>
