97f5a950436a53809655cfaf3d529d50c9f5ce30
gaze9999 <gaze9999@livemail.tw>
2019-12-22 03:04:16 +0800

    add warning bar

---
+++ tpl/tpl-warning.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="./src/sass/warning.css">
+<div class="footer_warning fixed-bottom w-100 d-flex center-all">
+  <h3 class="text-center">飲酒過量有礙健康，未成年請勿飲酒，禁止酒駕</h3>
+</div>
