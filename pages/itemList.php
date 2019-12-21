0e8829bb8ea599e612b89d025ac4443432e2f8e6
gaze9999 <gaze9999@livemail.tw>
2019-12-22 03:02:08 +0800

    add main itemlist

---
+++ pages/itemList.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require_once('./db.inc.php');
+require_once('./tpl/tpl-html-head.php');
+require_once('./tpl/tpl-header.php');
+?>
+
+<?php
+require_once('./tpl/tpl-footer.php');
+require_once('./tpl/tpl-html-foot.php');
+?>
