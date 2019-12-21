9335bce765e15f479aee27e10df20477d6aaad88
gaze9999 <gaze9999@livemail.tw>
2019-12-22 03:01:19 +0800

    add main and warning

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 session_start();
 require_once('./db.inc.php');
 require_once('./tpl/tpl-html-head.php');
-require_once('./tpl/tpl-header.php'); 
-require_once('./tpl/main.php');
-require_once('./tpl/tpl-footer.php'); 
-require_once('./tpl/tpl-html-foot.php'); 
+require_once('./tpl/tpl-header.php');
+require_once('./pages/main.php');
+require_once('./tpl/tpl-warning.php');
+require_once('./tpl/tpl-footer.php');
+require_once('./tpl/tpl-html-foot.php');
 ?>
