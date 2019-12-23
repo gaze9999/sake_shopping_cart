4408908af24a209823c62d1a9e8909cc0be7e8ac
gaze9999@iii <gaze9999@livemail.tw>
2019-12-23 16:39:41 +0800

    change item list class name

--- itemList.php
+++ itemList.php
@@ -15,24 +15,24 @@ require_once("./tpl/func-getRecursiveCategoryIds.php");
     </header>
     <div class="row itemList_frame">
 <!-- Catagory Tree -->
-      <aside class="col-md-2 col-sm-3">
+      <aside class="col-md-2 col-sm-3 itemList_tree">
         <?php buildTree($pdo, 1); ?>
       </aside>
 
 <!-- main field -->
-      <section class="col-md-8 col-sm-6 d-flex flex-wrap item_list">
+      <section class="col-md-8 col-sm-6 d-flex flex-wrap itemList_list">
         <?php require_once('./tpl/tpl-itemlist.php'); ?>
         <!-- <?php for($i=0;$i<20;$i++) {require('./tpl/tpl-itemcard.php'); }; ?> -->
       </section>
 
 <!-- filter -->
-      <aside class="col-md-2 col-sm-3">
+      <aside class="col-md-2 col-sm-3 itemList_filter">
         <div class="input-group mb-3">
           <input type="text" class="form-control" placeholder="商品名稱" aria-label="itemName" aria-describedby="basic-addon1">
         </div>
         <p>
           <label for="amount">商品價格範圍:</label>
-          <input type="text" id="amount" readonly placeholder="xxxxxxx">
+          <!-- <input type="text" id="amount" readonly placeholder="xxxxxxx"> -->
         </p>
       </aside>
     </div>
--- main.php
+++ main.php
@@ -6,7 +6,7 @@
           <h1 class="text-center">商品一覽</h1>
         </div>
       </header>
-      <section class="col-sm-12 col-md-12 col-xl-12 d-flex flex-wrap item_list">
+      <section class="col-sm-12 col-md-12 col-xl-12 d-flex flex-wrap itemList_list">
         <?php for($i=0;$i<20;$i++) {require('./tpl/tpl-itemcard.php'); }; ?>
       </section>
     </div>
