d3cdd1d6abad76771581158d53fb133f7416b6e2
gaze9999@iii <gaze9999@livemail.tw>
2019-12-23 15:38:56 +0800

    block pref

--- tpl/func-buildTree.php
+++ tpl/func-buildTree.php
@@ -8,12 +8,13 @@ function buildTree($pdo, $parentId = 1){
 
     if($stmt->rowCount() > 0) {
         echo "<dl>";
+        echo "<dt>地方";
         $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
         for($i = 0; $i < count($arr); $i++) {
-            echo "<dt>";
-            echo "<a href='./itemList.php?categoryId={$arr[$i]['rId']}'>{$arr[$i]['regionName']}</a>";
-            TreePref($pdo, $arr[$i]['rId']);
-            echo "</dt>";
+            echo "<dd>";
+            echo "<a href='./itemList.php?cId={$arr[$i]['rId']}'>{$arr[$i]['regionName']}</a>";
+            echo "</dd>";
+            // TreePref($pdo, $arr[$i]['rId']);
         }
         echo "</dl>";
     }
@@ -28,12 +29,17 @@ function TreePref($pdo, $prefId = 1){
     $stmt = $pdo->prepare($sql);
     $arrParam = [$prefId];
     $stmt->execute($arrParam);
+    // $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // echo "<pre>";
+    // print_r($arr);
+    // echo "</pre>";
+    // exit;
 
     if($stmt->rowCount() > 0) {
         $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        for($i = 0; $i < count($arr); $i++) {            
+        for($i = 0; $i < count($arr); $i++) {
             echo "<dd>";
-            echo "<a href='./itemList.php?categoryId={$arr[$i]['pId']}'>{$arr[$i]['prefName']}</a>";
+            echo "<a href='./itemList.php?cId={$arr[$i]['rrId']}&pId={$arr[$i]['pId']}'>{$arr[$i]['prefName']}</a>";
             echo "</dd>";
         }
     }
