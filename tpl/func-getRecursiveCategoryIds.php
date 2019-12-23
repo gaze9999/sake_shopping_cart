b8a0e687525604952b65d02d9ff7282f5cfe5973
gaze9999@iii <gaze9999@livemail.tw>
2019-12-23 16:40:17 +0800

    change sql var

--- tpl/func-getRecursiveCategoryIds.php
+++ tpl/func-getRecursiveCategoryIds.php
@@ -2,17 +2,17 @@
 //搭配全域變數，遞迴取得上下階層的 id 字串集合
 function getRecursiveCategoryIds($pdo, $categoryId){
     global $strCategoryIds;
-    $sql = "SELECT `categoryId`
-            FROM `categories` 
-            WHERE `categoryParentId` = ?";
+    $sql = "SELECT `rId`, `regionName`
+            FROM `sake_regions` 
+            WHERE `rId` = ? ";
     $stmt = $pdo->prepare($sql);
     $arrParam = [$categoryId];
     $stmt->execute($arrParam);
     if($stmt->rowCount() > 0) {
         $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
         for($i = 0; $i < count($arr); $i++) {
-            $strCategoryIds.= ",".$arr[$i]['categoryId'];
-            getRecursiveCategoryIds($pdo, $arr[$i]['categoryId']);
+            $strCategoryIds.= ",".$arr[$i]['rId'];
+            getRecursiveCategoryIds($pdo, $arr[$i]['rId']);
         }
     }
 }
