fefa4ab7b386ebfdc89f4a43f526c7c04d978097
gaze9999@iii <gaze9999@livemail.tw>
2019-12-23 14:27:28 +0800

    add comments split

--- itemList.php
+++ itemList.php
@@ -14,21 +14,28 @@ require_once("./tpl/func-getRecursiveCategoryIds.php");
       </div>
     </div>
     <div class="row">
-      <div class="col-md-2 col-sm-3"><?php buildTree($pdo, 1); ?></div>
+<!-- Catagory Tree -->
+      <div class="col-md-2 col-sm-3">
+        <?php buildTree($pdo, 1); ?>
+      </div>
+
+<!-- main field -->
       <div class="col-md-8 col-sm-6 d-flex flex-wrap item_list">
         <?php for($i=0;$i<20;$i++) {require('./tpl/tpl-itemcard.php'); }; ?>
       </div>
+
+<!-- filter -->
       <div class="col-md-2 col-sm-3">
         <div class="input-group mb-3">
-
           <input type="text" class="form-control" placeholder="商品名稱" aria-label="itemName" aria-describedby="basic-addon1">
         </div>
         <p>
           <label for="amount">商品價格範圍:</label>
-          <input type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
+          <input type="text" id="amount" readonly placeholder="xxxxxxx">
         </p>
         <div id="slider-range"></div>
       </div>
+
     </div>
   </div>
 </main>
