fea29ddca5ac499ffea22f161234c834a2e475dd
gaze9999 <gaze9999@livemail.tw>
2019-12-24 02:38:44 +0800

    add php echoes

--- tpl/tpl-itemcard.php
+++ tpl/tpl-itemcard.php
@@ -1,12 +1,12 @@
 <div class="card shadow-sm item_card">
   <div class="card-img-top d-flex center-all">
     <a class="" href="">
-      <img class="img-fluid item_card_img" src="./img/items/item_20191216030246.jpg">
+      <img class="img-fluid item_card_img" src="<?php echo "./img/items/item_20191216030246.jpg"; ?>">
     </a>
   </div>
   <div class="card-body d-flex center-all flex-column">
-    <p class="card-text">ITEM</p>
-    <p class="card-text">價格：500</p>
-    <small class="card-text">上架日期：1970-01-01</small>
+    <p class="card-text"><?php echo $_POST['itemName']; ?></p>
+    <p class="card-text">價格：<?php echo $_POST['price']; ?></p>
+    <!-- <small class="card-text">上架日期：1970-01-01</small> -->
   </div>
 </div>
---
+++ tpl/tpl-itemlist.php
@@ -0,0 +1,3 @@
+<?php  ?>
+
+<!-- require('./tpl/tpl-itemcard.php'); -->
