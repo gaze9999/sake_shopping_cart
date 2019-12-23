b311488301a68671fb97b658f784e17218227e47
gaze9999 <gaze9999@livemail.tw>
2019-12-23 21:37:40 +0800

    link data

--- tpl/tpl-header.php
+++ tpl/tpl-header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg fixed-top">
     <div class="nav_logo d-flex center-all">
         <header class="h-100">
-            <a class="navbar-brand d-flex center-all h-100" href="./index.php">
+            <a class="navbar-brand d-flex center-all h-100" name="nav_a_home" href="./">
                 <h1>本格の酒</h1>
                 <img class="img-fluid d-block" src="./img/icons/logo.svg">
             </a>
@@ -14,28 +14,28 @@
     </div>
     <div id="my-nav" class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="">地圖</a>
+            <li class="nav-item active" data-page="map">
+                <a class="nav-link" data-page="map">地圖</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">酒造介紹</a>
+                <a class="nav-link" data-page="breweries">酒造介紹</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="./itemList.php">商品列表</a>
+                <a class="nav-link" data-page="itemList" href="./itemList.php">商品列表</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">清酒知識+</a>
+                <a class="nav-link" data-page="knowledge">清酒知識+</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">新手專區</a>
+                <a class="nav-link" data-page="newCome">新手專區</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">關於我們</a>
+                <a class="nav-link" data-page="about">關於我們</a>
             </li>
         </ul>
     </div>
     <div class="nav_searchbar mx-5">
-        <span class="input-group-text" id="list_searchbar">Search</span>
+        <input class="header_searchbar" placeholder="Search"></input>
     </div>
     <div class="nav_function">
         <a class="nav_cart px-2" href="">購物車</a>
