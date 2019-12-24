cecf7ae2c3e49f2d0ff4d245f9181dfeb2e87926
gaze9999@iii <gaze9999@livemail.tw>
2019-12-24 14:23:16 +0800

    finished put

--- src/js/itemlistGetlist.js
+++ src/js/itemlistGetlist.js
@@ -2,53 +2,40 @@ let treeId = $('.tree_id');
 let cId = treeId.data('cid')
 let pId = treeId.data('pid')
 
-data = {
-  cid: cId+"",
-  pid: pId+""
+// data = treeId.data()
+data = JSON.stringify(treeId.data())
+// console.log(data)
+
+function getItem() {
+  fetch('./tpl/func-getItems.php', {
+    method: "PUT",
+    headers: {'Content-Type': 'application/json'},
+    body: data
+  }).then(res => {
+    if (res.status >= 200 && res.status < 300) {
+      return res.json();
+      // return res.text();
+      // return res.status;
+      // return res;
+    } else {
+      let error = new Error(response.statusText)
+      error.response = response;
+      throw error.Content-Type;
+    }
+
+  }).then(json => {
+    console.log(json);
+
+  }).then(data => {
+    console.log(data)
+
+  }).catch(error => {
+    // console.log('request failed', error);
+    // return error.response.json();
+
+  }).then(errorData => {
+    // console.log(errorData);
+  });
 }
 
-console.log(JSON.stringify(data))
-// console.log(JSON.stringify(treeId.data()))
-
-// $.ajax({
-//   url: "./tpl/func-getItems.php",
-//   type: "POST",
-//   method: "POST",
-//   // async: true,
-//   dataType: "json",
-//   contentType: "application/json;charset=utf-8",
-//   data: JSON.stringify(treeId.data()),
-//   // data:{
-//   //   cid: treeId.data('cid'),
-//   //   pid: treeId.data('pid')
-//   // }
-// }).done(function(json){
-//   console.log(json.info)
-// }).fail(function(data, jqXHR , textStatus){
-//   // console.log(data)
-//   console.log(textStatus)
-// })
-
-fetch('./tpl/func-getItems.php', {
-  method: "POST",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(treeId.data())
-}).then(function checkStatus(response) {
-  if (response.status >= 200 && response.status < 300) {
-      return response.json();
-  } else {
-      var error = new Error(response.statusText)
-      error.response = response;
-      throw error;
-  }
-}).then(function(json) {
-    console.log(json.info);
-}).catch(function(error) {
-    console.log('request failed', error);
-    return error.response.json();
-}).then(function(errorData){
-    console.log(errorData);
-});
+getItem()
--- tpl/func-getItems.php
+++ tpl/func-getItems.php
@@ -1,10 +1,17 @@
 <?php
 require_once('../db.inc.php');
+global $forList;
 
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
-exit;
+// echo json_encode($_SERVER['input']);
+// echo json_encode($_POST);
+$data = json_decode(file_get_contents('php://input'), true);
+
+// echo "<pre>";
+// print_r($data);
+// print_r($_POST);
+// print_r($_SERVER);
+// echo "</pre>";
+// exit;
 
 $sqlAll = "SELECT i.`sId`, i.`itemName`, i.`breId`, i.`vId`, i.`nihonshudo`, i.`capacity`, i.`price`, i.`discount`, b.`bId`, b.`breName`, b.`rId`, b.`pId`, p.`pId`, p.`prefName`, p.`rId`, r.`rId`, r.`regionName`, v.`vId`, v.`varieties`, v.`vCatag`, v.`alchoholAdd`, v.`seimaibuai` 
            FROM `sake_items` as i JOIN `sake_breweries` as b JOIN `sake_prefectures` as p JOIN `sake_regions` as r JOIN `sake_varieties` as v 
@@ -12,13 +19,27 @@ $sqlAll = "SELECT i.`sId`, i.`itemName`, i.`breId`, i.`vId`, i.`nihonshudo`, i.`
 
 $sql = "SELECT i.`sId`, i.`itemName`, i.`breId`, b.`bId`, b.`breName`, b.`rId`, r.`rId`, r.`regionName` 
         FROM `sake_items` as i JOIN `sake_breweries` as b JOIN `sake_regions` as r
-        WHERE r.`rId` in (?) ";
+        WHERE r.`rId` = ? AND b.`rId` = ? AND b.`bId` = i.`breId`
+        GROUP BY i.`itemName` 
+        ORDER BY i.`sId` ";
 
-$arrParam = [$_POST['cid']];
+$arrParam = [$data['cid'], $data['cid']];
 $stmt = $pdo->prepare($sql);
 $stmt->execute($arrParam);
 
 if($stmt->rowCount() > 0) {
   $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  // echo $stmt->rowCount();
+  // for ($i=0;$i<$stmt->rowCount();$i++) {
+  //   echo "<pre>";
+  //   print_r($arr[$i]);
+  //   echo "</pre>";
+  // }
+  $forList = json_encode($arr);
+  
+  // echo "<pre>";
+  // print_r($forList);
+  // print_r($arr);
+  // echo "</pre>";
 }
 ?>
