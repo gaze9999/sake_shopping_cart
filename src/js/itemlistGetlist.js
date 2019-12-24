f7b0714b84b9d2faa0b59a6adc675d70af6b54dc
gaze9999@iii <gaze9999@livemail.tw>
2019-12-24 16:26:44 +0800

    remove test lines

--- src/js/itemlistGetlist.js
+++ src/js/itemlistGetlist.js
@@ -1,10 +1,10 @@
-let treeId = $('.tree_id');
+let treeId = $('.tree_id'), jsonData;
 
 // data = treeId.data()
 data = JSON.stringify(treeId.data())
 // console.log(data)
 
-function getItem() {
+function getItem(num) {
   fetch('./tpl/func-getItems.php', {
     method: "PUT",
     headers: {'Content-Type': 'application/json',
@@ -13,10 +13,6 @@ function getItem() {
   }).then(res => {
     if (res.status >= 200 && res.status < 300) {
       return res.json();
-      // return res.text();
-      // return res.status;
-      // return res;
-      // console.log(res.json())
     } else {
       let error = new Error(res.statusText);
       error.response = res;
@@ -24,15 +20,18 @@ function getItem() {
     }
 
   }).then(json => {
-    console.log(json)
+    console.log(json[num])
 
   }).catch(error => {
     console.log('request failed:', error);
     // return error.response.json();
-
-  }).then(errorData => {
-    // console.log(errorData);
   });
 }
 
-getItem()
+// getItem()
+
+function pasteData(){
+  console.log(getItem(10))
+}
+
+pasteData()
---
+++ src/js/itemlistRegions.js
@@ -0,0 +1,7 @@
+// let itemcId = $('.itemList_tree');
+// let itemList = $('.itemList_list');
+
+// itemcId.on('click', 'a', function(){
+//     getItem()
+//     itemList.html(getItem())
+// });
