51991a2786483c9a40c90650f3f5c06a6c67e0ec
gaze9999@iii <gaze9999@livemail.tw>
2019-12-26 13:42:47 +0800

    add

---
+++ itemDetails.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require_once('./db.inc.php');
+require_once('./tpl/tpl-html-head.php');
+require_once('./tpl/tpl-header.php');
+require_once('./tpl/tpl-warning.php');
+require_once('./tpl/tpl-footer.php');
+require_once('./tpl/tpl-html-foot.php');
+?>
