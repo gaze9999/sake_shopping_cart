d840715209d36c3b619a06478de61b1fd13e62d3
gaze9999 <gaze9999@livemail.tw>
2019-12-27 23:21:23 +0800

    add template

--- itemDetails.php
+++ itemDetails.php
@@ -3,6 +3,13 @@ session_start();
 require_once('./db.inc.php');
 require_once('./tpl/tpl-html-head.php');
 require_once('./tpl/tpl-header.php');
+?>
+
+<main>
+  
+</main>
+
+<?php
 require_once('./tpl/tpl-warning.php');
 require_once('./tpl/tpl-footer.php');
 require_once('./tpl/tpl-html-foot.php');
