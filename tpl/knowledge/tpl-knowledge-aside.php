dc6b12c29eca9b75385ea0143bd46997331d3fae
gaze9999 <gaze9999@livemail.tw>
2019-12-29 22:35:27 +0800

    put contents in

---
+++ tpl/knowledge/tpl-knowledge-aside.php
@@ -0,0 +1,72 @@
+<aside class="col-md-4 col-sm-4 h-100 d-flex flex-column">
+    <div class="popular-post">
+        <h2 class="news">- 最新消息 -</h2>
+        <div class="post-content" data-aos="flip-up" data-aos-delay="200">
+
+            <div class="post-image">
+                <div>
+                    <img class="img-fluid" src="./img/knowledge/aside/gn.jpg" class="img gn" alt="blog1">
+                </div>
+                <div class="post-info flex-row">
+                    <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;January 1,
+                        2020</span>
+                </div>
+            </div>
+            <div class="post-title">
+                <a href="#">新年送禮好選擇</a>
+            </div>
+        </div>
+
+        <div class="post-content" data-aos="flip-up" data-aos-delay="300">
+            <div class="post-image">
+                <div>
+                    <img class="img-fluid" src="./img/knowledge/aside/spring.jpg" class="img" alt="blog1">
+                </div>
+                <div class="post-info flex-row">
+                    <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;January 1,
+                        2020</span>
+                </div>
+            </div>
+            <div class="post-title">
+                <a href="#">春天了，想跟你去日本喝清酒</a>
+            </div>
+        </div>
+
+        <div class="post-content" data-aos="flip-up" data-aos-delay="400">
+            <div class="post-image">
+                <div>
+                    <img class="img-fluid" src="./img/knowledge/aside/bogfo01.jpg" class="img" alt="blog1">
+                </div>
+                <div class="post-info flex-row">
+                    <span><i class="fas fa-calendar-alt text-gray"></i>&nbsp;&nbsp;January 1,
+                        2020</span>
+                </div>
+            </div>
+            <div class="post-title">
+                <a href="#">春季特銷，全品項買一送一</a>
+            </div>
+        </div>
+    </div>
+    
+    <div class="newsletter" data-aos="fade-up" data-aos-delay="300">
+        <h2>- 訂閱小知識 -</h2>
+        <div class="form-element">
+            <input type="text" class="input-element" placeholder="Email">
+            <button class="btn-hyh form-btn-hyh">送出</button>
+        </div>
+    </div>
+
+    <div class="popular-tags">
+        <h2>熱門標籤排行看這邊</h2>
+        <div class="tags flex-row">
+            <span class="tag" data-aos="flip-up" data-aos-delay="100">#清酒</span>
+            <span class="tag" data-aos="flip-up" data-aos-delay="200">#日本酒</span>
+            <span class="tag" data-aos="flip-up" data-aos-delay="300">#獺祭</span>
+            <span class="tag" data-aos="flip-up" data-aos-delay="400">#山田錦</span>
+            <span class="tag" data-aos="flip-up" data-aos-delay="500">#酒造</span>
+            <span class="tag" data-aos="flip-up" data-aos-delay="600">#日本酒推薦</span>
+            <span class="tag" data-aos="flip-up" data-aos-delay="700">#十四代</span>
+            <!-- <span class="tag" data-aos="flip-up" data-aos-delay="800">project</span> -->
+        </div>
+    </div>
+</aside>
---
+++ tpl/knowledge/tpl-knowledge-content.php
@@ -0,0 +1,65 @@
+<div class="col-md-8 col-sm-8 d-flex flex-column center_all">
+  <article class="know_post" data-aos="zoom-in" data-aos-delay="200">
+    <figure class="know_post_img d-flex center_all">
+      <img class="img-fluid" src="./img/knowledge/contents/shantg.jpg" alt="blog1">
+    </figure>
+      <h4 class="know_post_title"><a>關於酒米，你該知道的幾件事</a></h4>
+    <section class="know_post_content">
+      大家絕對都知道，釀造清酒基本上只需要水跟米。
+      為了釀造出獨一無二的清酒，原料的選擇就變得很重要，
+      這也是為什麼市面上會有超過90種的酒米的原因。
+      每一種酒米都有著自己的特點，藉著了解他們的特點，
+      在購買、選擇清酒時，你會更容易選到適合自己的清酒......
+    </section>
+      <button class="know_btn know_post_btn">閱讀更多 &nbsp; <i class="fas fa-arrow-right"></i></button>
+  </article>
+
+<hr>
+
+  <article class="know_post" data-aos="zoom-in" data-aos-delay="200">
+    <figure class="know_post_img d-flex center_all">
+      <img class="img-fluid" src="./img/knowledge/contents/ng.jpg" alt="blog1">
+    </figure>
+      <h4 class="know_post_title"><a>吟釀？大吟釀？純米吟釀？到底是什麼？</a></h4>
+    <section class="know_post_content">
+      剛接觸日本酒的人，一定會被一些專有名詞給愣住，例如精米步合、吟釀、純米酒...等。
+      雖然重點是在於品嚐日本酒的醍醐味，但有些名詞搞懂後，
+      會更能體會酒款的風格與重點，酒也會覺得更好喝喔......
+    </section>
+      <button class="know_btn know_post_btn">閱讀更多 &nbsp; <i class="fas fa-arrow-right"></i></button>
+  </article>
+
+  <hr>
+
+  <article class="know_post" data-aos="zoom-in" data-aos-delay="200">
+    <figure class="know_post_img d-flex center_all">
+      <img class="img-fluid" src="./img/knowledge/contents/gl.jpg" alt="blog1">
+    </figure>
+      <h4 class="know_post_title"><a>答應我，來一趟酒造之旅吧！</a></h4>
+    <section class="know_post_content">
+      探訪的對象是產量最大的芋燒酎，使用地瓜做成的燒酎，
+      香氣獨特，強烈濃郁，以鹿兒島跟宮崎為代表。
+      今年探訪的對象分別是產量第二跟第三大「麥燒酎」跟「米燒酎」，
+      這次總共會去三站，第一站是熊本人吉的「織月酒造」，
+      早上探訪酒造，下午瀏覽在地風光，晚上拎著酒吃在地美食，
+      就是一天完美的燒酎小旅行。
+    </section>
+      <button class="know_btn know_post_btn">閱讀更多 &nbsp; <i class="fas fa-arrow-right"></i></button>
+  </article>
+
+<hr>
+
+  <article class="know_post" data-aos="zoom-in" data-aos-delay="200">
+    <figure class="know_post_img d-flex center_all">
+      <img class="img-fluid" src="./img/knowledge/contents/yn.jpg" alt="blog1">
+    </figure>
+      <h4 class="know_post_title"><a>用少女口水釀的酒？什麼是口嚼酒？</a></h4>
+    <section class="know_post_content">
+      看過新海誠電影《你的名字》的朋友就知道，電影裡面出現了口嚼酒
+      (口噛みノ酒) 這種奇妙的酒類。根據史書《大隅國風土記》描述，
+      大隅國 (現今的鹿兒島縣東部)的居民會將煮好的米咀嚼後吐進容器中，
+      放置一晚發酵成酒。
+    </section>
+      <button class="know_btn know_post_btn">閱讀更多 &nbsp; <i class="fas fa-arrow-right"></i></button>
+  </article>
+</div>
