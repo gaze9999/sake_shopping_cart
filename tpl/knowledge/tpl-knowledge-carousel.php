1b1adae3c0d72c3589499dcd05694e3c82364b20
gaze9999 <gaze9999@livemail.tw>
2019-12-29 22:35:03 +0800

    move to folder

---
+++ tpl/knowledge/tpl-knowledge-carousel.php
@@ -0,0 +1,41 @@
+<div class="col-md-12 col-sm-12 w-100 d-flex flex-column center_all">
+  <h2>旅遊資訊</h2>
+  <div class="owl-carousel owl-theme know_carousel_post">
+    <div class="know_carousel_content" data-aos="fade-right" data-aos-delay="200">
+      <img src="./img/knowledge/travel/九重夢大吊橋.jpg" alt="post-1">
+      <div class="know_carousel_title">
+        <h3>九重夢大吊橋</h3>
+        <button class="know_btn carousel_title_btn">More</button>
+        <!-- <span>2 minutes</span> -->
+      </div>
+    </div>
+    <div class="know_carousel_content" data-aos="fade-in" data-aos-delay="200">
+        <img src="./img/knowledge/travel/長崎縣豪斯登堡.jpg" alt="post-2">
+        <div class="know_carousel_title">
+          <h3>長崎縣豪斯登堡</h3>
+          <button class="know_btn carousel_title_btn">More</button>
+          <!-- <span>2 minutes</span> -->
+        </div>
+    </div>
+    <div class="know_carousel_content" data-aos="fade-left" data-aos-delay="200">
+      <img src="./img/knowledge/travel/山形城跡 霞城公園.jpg" alt="post-3">
+      <div class="know_carousel_title">
+        <h3>山形城跡 霞城公園</h3>
+        <button class="know_btn carousel_title_btn">More</button>
+        <!-- <span>2 minutes</span> -->
+      </div>
+    </div>
+    <div class="know_carousel_content" data-aos="fade-right" data-aos-delay="200">
+      <img src="./img/knowledge/travel/高知縣高知城.jpg" alt="post-4">
+      <div class="know_carousel_title">
+        <h3>高知縣高知城</h3>
+        <button class="know_btn carousel_title_btn">More</button>
+        <!-- <span>2 minutes</span> -->
+      </div>
+    </div>
+  </div>
+  <div class="owl-navigation">
+    <span class="owl-nav-prev owl_nav_btn"><i class="fas fa-chevron-left"></i></span>
+    <span class="owl-nav-next owl_nav_btn"><i class="fas fa-chevron-right"></i></span>
+  </div>
+</div>
