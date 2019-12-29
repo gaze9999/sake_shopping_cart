f80b793fa6172d1a55ce3938cfeb1689ab8fafdd
gaze9999 <gaze9999@livemail.tw>
2019-12-29 22:36:07 +0800

    fixed typo

--- src/js/owlCarousel.js
+++ src/js/owlCarousel.js
@@ -1,26 +1,27 @@
 let owlRes = {
   0: {
-      items: 1
+    items: 1
   },
   320: {
-      items: 1
+    items: 1
   },
   560: {
-      items: 2
+    items: 2
   },
   960: {
-      items: 3
+    items: 3
   }
 }
 
 $(document).ready(() => {
   // owl-crousel for blog
   $('.owl-carousel').owlCarousel({
-      loop: true,
-      autoplay: false,
-      autoplayTimeout: 3000,
-      dots: false,
-      nav: true,
-      navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-      responsive: owlRes
-  });
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: owlRes
+    })
+})
