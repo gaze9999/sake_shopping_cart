1e5e6c71775cc2f3182c6a5793e78c47249f6d4a
gaze9999 <gaze9999@livemail.tw>
2020-01-02 21:52:17 +0800

    add news section and add title animation

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
   <div class="container-fluid d-flex flex-column center_all page_main">
     <div class="row w-100">
       <section class="col-sm-12 col-md-12 col-xl-12 w-100 d-flex center_all main_logoSec">
-        <header class="h-100">
+        <header class="h-100" data-aos="zoom-out" data-aos-duration="1000">
           <figure class="d-flex center_all">
             <img class="img-fluid d-block main_logo" src="./img/icons/main_icon.svg" alt="本格の酒">
             <h1>本 格 の 酒</h1>
@@ -11,12 +11,20 @@
         </header>
       </section>
 
-      <div class="col-sm-12 col-md-12 col-xl-12 w-100 d-flex center_all main_article_center">
-        <article class="w-100 d-flex center_all main_article_about">自1900年成立（明治33年）以來，<br/>高橋清酒釀酒廠一直致力<br/>於在位於熊本縣最南端的人吉和久馬地區<br/>製作正宗的米燒酒。<br/>生產的不僅是稻米燒酒，還包括有400多年曆史的熊馬燒酒。<br/>熊麻燒酒與長崎的讚岐燒酒，鹿兒島的薩摩燒酒和沖繩的<br/>琉球琉森燒酒相比，這是只有四個正宗燒酒的品牌。</article>
-      </div>
+      <section class="col-sm-12 col-md-12 col-xl-12 w-100 d-flex center_all main_article_center">
+        <article class="w-100 d-flex center_all main_article_about" data-aos="zoom-out" data-aos-duration="1000">自1900年成立（明治33年）以來，<br/>高橋清酒釀酒廠一直致力<br/>於在位於熊本縣最南端的人吉和久馬地區<br/>製作正宗的米燒酒。<br/>生產的不僅是稻米燒酒，還包括有400多年曆史的熊馬燒酒。<br/>熊麻燒酒與長崎的讚岐燒酒，鹿兒島的薩摩燒酒和沖繩的<br/>琉球琉森燒酒相比，這是只有四個正宗燒酒的品牌。</article>
+      </section>
 
-      <section class="col-sm-12 col-md-12 col-xl-12 w-100 d-flex center_all main_sec3">
-        <h2>最新消息</h2>
+      <section class="col-sm-12 col-md-12 col-xl-12 w-100 d-flex flex-column center_all main_news">
+        <figure>
+          <img src="./img/icons/main_icon.svg" alt="">
+          <h2>最新消息</h2>
+        </figure>
+        <?php require_once('./tpl/main/tpl-main-news.php'); ?>
+      </section>
+
+      <section class="col-sm-12 col-md-12 col-xl-12 w-100 d-flex flex-column center_all main_knowledge">
+        <!-- <?php require_once('./tpl/main/tpl-main-knowledge.php'); ?> -->
       </section>
     </div>
   </div>
---
+++ tpl/main/tpl-main-news.php
@@ -0,0 +1,20 @@
+<div class="row main_news_items">
+  <article class="news_card col-lg-2 col-sm-12" data-aos="zoom-out-up">
+    <figure class="d-flex flex-column center_all">
+      <img src="./img/main/news/news1.jpg" alt="">
+      <figcaption class="col-lg-12 col-sm-3 col-8 py-3">自1900年成立（明治33年）以來，高橋清酒釀酒廠一直致力於在位於熊本縣最南端的人吉和久馬地區製作正宗的米燒酒。</figcaption>
+    </figure>
+  </article>
+  <article class="news_card col-lg-2 col-sm-12" data-aos="zoom-out-up">
+    <figure class="d-flex flex-column center_all">
+      <img src="./img/main/news/news2.jpg" alt="">
+      <figcaption class="col-lg-12 col-sm-3 col-8 py-3">自1900年成立（明治33年）以來，高橋清酒釀酒廠一直致力於在位於熊本縣最南端的人吉和久馬地區製作正宗的米燒酒。</figcaption>
+    </figure>
+  </article>
+  <article class="news_card col-lg-2 col-sm-12" data-aos="zoom-out-up">
+    <figure class="d-flex flex-column center_all">
+      <img src="./img/main/news/news3.jpg" alt="">
+      <figcaption class="col-lg-12 col-sm-3 col-8 py-3">自1900年成立（明治33年）以來，高橋清酒釀酒廠一直致力於在位於熊本縣最南端的人吉和久馬地區製作正宗的米燒酒。</figcaption>
+    </figure>
+  </article>
+</div>
