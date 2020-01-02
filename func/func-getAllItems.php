bf9b20e95c99dd203ac8893917fb6081f5914d85
gaze9999 <gaze9999@livemail.tw>
2020-01-02 14:27:48 +0800

    add

---
+++ func/func-getAllItems.php
@@ -0,0 +1,20 @@
+<?php
+require_once('./db.inc.php');
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$sql = "SELECT i.`sId`, i.`itemName`, i.`breId`, i.`vId`, b.`bId`, b.`breName`, b.`rId`, r.`rId`, r.`regionName`, b.`pId`, p.`pId`, p.`prefName`, v.`vId`, v.`varieties`, v.`vCatId`, v.`vCatag`, v.`vcId`
+FROM `sake_items` as i JOIN `sake_breweries` as b JOIN `sake_regions` as r JOIN `sake_prefectures` as p JOIN `sake_varieties` as v 
+ON i.`breId` = b.`bId` AND i.`vId` = v.`vId` AND b.`rId` = r.`rId` AND p.`pId` = b.`pId` ";
+
+$sql.= "GROUP BY i.`itemName` 
+ORDER BY i.`sId` ";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+if($stmt->rowCount() > 0) {
+$arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
+};
+$result = urldecode(json_encode($arr));
+echo $result;
+?>
