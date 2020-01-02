5f832e8167cb4bdd697bd213468db5cbe02804a7
gaze9999 <gaze9999@livemail.tw>
2020-01-02 12:13:43 +0800

    change rids to array

--- src/js/itemlistBtn.js
+++ src/js/itemlistBtn.js
@@ -1,31 +1,28 @@
 getVarieties()
 getTree()
 
-// itemTree.on('mouseup', '.tree_btn', function() {
-//   vid  = ""
-//   vcat = ""
-//   rids = ""
-//   vid  = $(this).data('vid')
-//   vcat = $(this).data('vcat')
-//   $('.filter_region .filter_checkbox.checked').each( function() {
-//     let Selected = $(this).data('region')
-//     rids += Selected + ", "
-//   });
-//   rids = rids.substring(0, rids.length-2)
-//   getVarieties(vcat, vid, "", rids)
-// })
+itemTree.on('mouseup', '.tree_btn', function() {
+  vid  = ""
+  vcat = ""
+  rids = ""
+  vid  = $(this).data('vid')
+  vcat = $(this).data('vcat')
+  $('.filter_region .filter_checkbox.checked').each( function() {
+    let Selected = $(this).data('region')
+    rids += Selected + ", "
+  });
+  rids = rids.substring(0, rids.length-2)
+  getVarieties(vcat, vid, "", rids)
+})
 
-// filterRCbox.on('change', function() {
-//   $(this).toggleClass('checked')
-//   let rids = ""
-//   $('.filter_region .filter_checkbox.checked').each( function() {
-//     let Selected = $(this).data('region')
-//     rids += Selected + ", "
-//   });
-//   rids = rids.substring(0, rids.length-2)
-//   filterItem(vcat, vid, "", rids)
-// })
-
-// itemTree.on('mouseup', '.tree_btn', function() {
-
-// }
+filterRCbox.on('change', function() {
+  $(this).toggleClass('checked')
+  let rids = []
+  $('.filter_region .filter_checkbox.checked').each( function() {
+    let Selected = $(this).data('region')
+    rids.push(Selected)
+  });
+  // rids = rids.substring(0, rids.length-2)
+  // console.log(rids)
+  filterItem(vcat, vid, "", rids)
+})
