b5e9a9e85de1d785b2788c19fcfc42857c6039a6
gaze9999@iii <gaze9999@livemail.tw>
2020-01-03 12:14:50 +0800

    add

---
+++ tpl/main/tpl-main-cloud.php
@@ -0,0 +1,7 @@
+<div class="position-absolute main_rec_cloud w-100 h-100">
+  <i class="position-absolute d-block cloud_1 cloud_bg_sm" data-aos="fade-right"></i>
+  <i class="position-absolute d-block cloud_2 cloud_bg_big" data-aos="fade-right"></i>
+  <i class="position-absolute d-block cloud_3 cloud_bg_big" data-aos="fade-left"></i>
+  <i class="position-absolute d-block cloud_4 cloud_bg_sm" data-aos="fade-left"></i>
+  <i class="position-absolute d-block cloud_5 cloud_bg_big" data-aos="fade-left"></i>
+</div>
