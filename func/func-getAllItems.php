0079ed0ddc0bbb9bba6f86fa3f54efe7da04f0a2
gaze9999 <gaze9999@livemail.tw>
2020-01-05 22:24:51 +0800

    add price

--- func/func-getAllItems.php
+++ func/func-getAllItems.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../db.inc.php');
 
-$sql = "SELECT i.`sId`, i.`itemName`, i.`breId`, i.`vId`, b.`bId`, b.`breName`, b.`rId`, r.`rId`, r.`regionName`, b.`pId`, p.`pId`, p.`prefName`, v.`vId`, v.`varieties`, v.`vCatId`, v.`vCatag`, v.`vcId`
+$sql = "SELECT i.`sId`, i.`itemName`, i.`breId`, i.`vId`, i.`price`, b.`bId`, b.`breName`, b.`rId`, r.`rId`, r.`regionName`, b.`pId`, p.`pId`, p.`prefName`, v.`vId`, v.`varieties`, v.`vCatId`, v.`vCatag`, v.`vcId`
         FROM `sake_items` as i JOIN `sake_breweries` as b JOIN `sake_regions` as r JOIN `sake_prefectures` as p JOIN `sake_varieties` as v 
         ON i.`breId` = b.`bId` AND i.`vId` = v.`vId` AND b.`rId` = r.`rId` AND p.`pId` = b.`pId` 
         GROUP BY i.`itemName` 
