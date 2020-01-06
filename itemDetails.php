57e1492125f9f62bae9a4d8463092819b9bde3b0
gaze9999 <gaze9999@livemail.tw>
2020-01-06 21:07:32 +0800

    add detail page

--- itemDetails.php
+++ itemDetails.php
@@ -5,8 +5,16 @@ require_once('./tpl/tpl-html-head.php');
 require_once('./tpl/tpl-header.php');
 ?>
 
-<main>
-  
+<main class="my-5 h-100 d-flex center_all">
+  <header class="row w-100 center_all detail_header">
+    <section class="col-md-12 col-sm-12 d-flex center_all detail_header_title">
+      <figure class="" data-aos="zoom-in" data-aos-delay="200">
+        <img class="" src="./img/test/pic051.png" alt="">
+        <h2 class="list_info_title">朝開酒藏</h2>
+      </figure>
+      <img data-aos="fade-left" data-aos-delay="200" src="./img/bgs/seigaiwa-sm.svg" alt="">
+    </section>
+  </header>
 </main>
 
 <?php
