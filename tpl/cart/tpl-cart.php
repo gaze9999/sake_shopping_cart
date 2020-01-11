4162700c303abd39016d26c4a7a17dfe6d58f8b8
gaze9999 <gaze9999@livemail.tw>
2020-01-11 17:09:33 +0800

    add cart tpl

---
+++ tpl/cart/tpl-cart.php
@@ -0,0 +1,25 @@
+<section>
+  <h4>購物車<i class="fa fa-shopping-cart"></i></h4>
+  <div class="d-flex flex-column">
+    <a href="#">ITEM1<span class="price">$</span></a>
+    <a href="#">ITEM2<span class="price">$</span></a>
+    <a href="#">ITEM3<span class="price">$</span></a>
+  </div>
+</section>
+
+<section>
+  <ul>
+    <li class="d-flex justify-content-between">            
+      <span class="">商品總額</span>
+      <span class="">$ 4,250</span>
+    </li>
+    <li class="d-flex justify-content-between">            
+      <span class="">運費</span>
+      <span class="">$ 150</span>
+    </li>
+    <li class="d-flex justify-content-between">            
+      <span class="">總金額</span>
+      <span class="">$ 4,400</span>
+    </li>
+  </ul>
+</section>
