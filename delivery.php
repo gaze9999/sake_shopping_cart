fa2125844ca59c47bc917abfb81661ba86dd47b7
gaze9999 <gaze9999@livemail.tw>
2020-01-12 03:13:53 +0800

    delivery layout done

--- delivery.php
+++ delivery.php
@@ -7,126 +7,125 @@ require_once('./tpl/tpl-header.php');
 <style>
   .navbar-dark, .navbar-nav, .nav-link, .navbar-brand { color: #000 !important}
 </style>
-<main class="container checkout_frame">
+<main class="container devilery_frame">
   <div class="row">
-    <form class="col-md-9" method="POST" action="">
-      <section>
-        <h2>配送方式</h2>
-        <a class="continue-cky"><i class="fas fa-chevron-left"></i> 返回上一步</a>
+    <form class="col-md-9 form-row" method="POST" action="">
+      <section class="form-group col-md-12 delivery_ship">
+        <hgroup class="d-flex justify-content-between align-items-center">
+          <h2>配送方式</h2>
+          <a>
+            <h5><i class="fas fa-chevron-left"></i> 返回上一步</h5>
+          </a>
+        </hgroup>
         <hr>
-        <button class="btn btn-dark">店到店</button>
-        <button class="btn btn-dark active">宅配</button>
+        <div class="d-flex center_all btn-group">
+          <input type="button" class="btn delivery_btn delivery_ship_p2p" value="店到店" data-ship="0">
+          <input type="button" class="btn delivery_btn delivery_ship_p2o" value="宅配" data-ship="1">
+        </div>
+        <input type="hidden" class="delivery_ship_selection" val="1">
       </section>
 
-      <section>
+      <section class="form-group col-md-12 delivery_order">
         <h2>訂購人</h2>
         <hr>
         <div class="form-row">
           <section class="form-group col-md-6">
-            <label for="">姓名</label>
-            <input type="text" class="form-control" maxlength="10" placeholder="請輸入中英文" required>
+            <label for="delivery_name">姓名</label>
+            <input type="text" class="form-control" id="delivery_name" maxlength="10" placeholder="請輸入中英文" required>
+            <label for="delivery_tel">聯絡手機</label>
+            <input type="tel" class="form-control" id="delivery_tel" placeholder="請輸入聯絡手機" name="phone" required>
           </section>
 
-          <section class="form-group col-md-6">
-            <label class="radio-cky">先生</label>
-            <input type="radio" value="male" name="gender" required>
-            <label class="radio-cky">小姐</label>
-            <input type="radio" value="female" name="gender" required>
-          </section>
-
-          <section class="form-group col-md-6">
-            <label for="">聯絡手機</label>
-            <input type="tel" class="form-control" placeholder="請輸入聯絡手機" name="phone" required>
+          <section class="form-group d-flex col-md-6 m-0 justify-content-between delivery_order_gender">
+            <div class="d-flex h-100 flex-grow-1 center_all btn-group">
+              <input type="button" class="btn delivery_btn delivery_gender" value="先生" data-gender="0">
+              <input type="button" class="btn delivery_btn delivery_gender" value="小姐" data-gender="1">
+            </div>
           </section>
         </div>
 
         <div class="form-row">
           <section class="form-group col-md-12">
-            <label for="">Email</label>
-            <input type="email" class="form-control" maxlength="60" placeholder="請輸入電子信箱" required>
+            <label for="delivery_order_email">Email</label>
+            <input type="email" class="form-control" id="delivery_order_email" maxlength="60" placeholder="請輸入電子信箱" required>
           </section>
-
-          <section class="form-group col-md-6 ">
-            <label for="">城市</label>
-            <select class="mr-sm-2 custom-select " placeholder="selectCity" required>
+          <section class="form-group col-md-6">
+            <label for="delivery_order_city">城市</label>
+            <select class="mr-sm-2 custom-select" id="delivery_order_city" placeholder="selectCity" required>
               <option value="19">宜蘭縣</option>
             </select>
           </section>
-
           <section class="form-group col-md-4">
-            <label for="">市區</label>
-            <select class="custom-select mr-sm-2" placeholder="selectState" required>
+            <label for="delivery_order_state">市區</label>
+            <select class="mr-sm-2 custom-select" id="delivery_order_state" placeholder="selectState" required>
               <option value=""></option>
             </select>
           </section>
-
           <section class="form-group col-md-2">
-            <label for="">郵遞區號</label>
-            <input type="text" class="form-control" required disabled>
+            <label for="delivery_order_post">郵遞區號</label>
+            <input type="text" id="delivery_order_post" class="form-control" required disabled>
           </section>
-
           <section class="form-group col-md-12">
-            <label for="">地址</label>
-            <input type="text" class="form-control" placeholder="請輸入詳細地址" required>
+            <label for="delivery_order_address">地址</label>
+            <input type="text" class="form-control" id="delivery_order_address" placeholder="請輸入詳細地址" required>
           </section>
         </div>
       </section>
 
-      <section>
-        <div class="shippingDetail-cky">
+      <section class="form-group col-md-12 delivery_reciver">
+        <div class="delivery_recive">
           <h3>收件人</h3>
           <hr>
           <section class="form-group col-md-12">
-            <div class="form-check">
-              <label class="form-check-label"><input type="checkbox" class="form-check-input">同訂購人</label>
+            <div class="form-check delivery_recive_same">
+              <label class="form-check-label">
+                <input type="checkbox" class="form-check-input">
+                同訂購人
+              </label>
             </div>
           </section>
 
           <section>
             <div class="form-row">
               <section class="form-group col-md-6">
-                <label for="">姓名</label>
-                <input type="text" class="form-control" maxlength="10" placeholder="請輸入中英文" required>
+                <label for="delivery_name">姓名</label>
+                <input type="text" class="form-control" id="delivery_name2" maxlength="10" placeholder="請輸入中英文" required>
+                <label for="delivery_tel">聯絡手機</label>
+                <input type="tel" class="form-control" id="delivery_tel2" placeholder="請輸入聯絡手機" name="phone" required>
               </section>
-
-              <section class="form-group col-md-6">
-                <label class="radio-cky">先生</label>
-                <input type="radio" value="male" name="gender" required>
-                <label class="radio-cky">小姐</label>
-                <input type="radio" value="female" name="gender" required>
-              </section>
-
-              <section class="form-group col-md-6">
-                <label for="">聯絡手機</label>
-                <input type="tel" class="form-control" placeholder="請輸入聯絡手機" name="phone" required>
+              <section class="form-group d-flex col-md-6 m-0 justify-content-between delivery_reviver_gender">
+                <div class="d-flex h-100 flex-grow-1 center_all btn-group">
+                  <input type="button" class="btn delivery_btn delivery_gender" value="先生" data-gender="0">
+                  <input type="button" class="btn delivery_btn delivery_gender" value="小姐" data-gender="1">
+                </div>
               </section>
             </div>
 
             <div class="form-row">
-              <section class="form-group col-md-6 ">
-                <label for="">城市</label>
-                <select class="mr-sm-2 custom-select " placeholder="selectCity" required>
+              <section class="form-group col-md-12">
+                <label for="delivery_reciver_email">Email</label>
+                <input type="email" class="form-control" id="delivery_reciver_email" maxlength="60" placeholder="請輸入電子信箱" required>
+              </section>
+              <section class="form-group col-md-6">
+                <label for="delivery_reciver_city">城市</label>
+                <select class="mr-sm-2 custom-select" id="delivery_reciver_city" placeholder="selectCity" required>
                   <option value="19">宜蘭縣</option>
                 </select>
               </section>
-
               <section class="form-group col-md-4">
-                  <label for="">市區</label>
-                  <select class="custom-select mr-sm-2" placeholder="selectState" required>
-                    <option value=""></option>
-                  </select>
+                <label for="delivery_reciver_state">市區</label>
+                <select class="mr-sm-2 custom-select" id="delivery_reciver_state" placeholder="selectState" required>
+                  <option value=""></option>
+                </select>
               </section>
-
               <section class="form-group col-md-2">
-                  <label for="">郵遞區號</label>
-                  <input type="text" class="form-control" required disabled>
+                <label for="delivery_reciver_post">郵遞區號</label>
+                <input type="text" id="delivery_reciver_post" class="form-control" required disabled>
               </section>
-
               <section class="form-group col-md-12">
-                <label for="">地址</label>
-                <input type="text" class="form-control" placeholder="請輸入詳細地址" required>
+                <label for="delivery_reciver_address">地址</label>
+                <input type="text" class="form-control" id="delivery_reciver_address" placeholder="請輸入詳細地址" required>
               </section>
-
               <section class="form-group col-md-12">
                 <div class="form-check">
                   <label class="form-check-label"><input type="checkbox" class="form-check-input">存為常用地址</label>
@@ -137,40 +136,50 @@ require_once('./tpl/tpl-header.php');
         </div>
       </section>
 
-      <section>
+      <section class="form-group col-md-12 delivery_temperature">
         <h3>配送方式</h3>
         <hr>
-        <div class="d-flex flex-column">
-          <label class="" for="delivery2"><input type="radio" id="delivery2" name="deliveryRadios" value="2" required>常溫配送</label>
-          <label class="" for="delivery3"><input type="radio" id="delivery3" name="deliveryRadios" value="3" required>低溫配送</label>
+        <div class="d-flex center_all btn-group">
+          <input type="button" class="btn delivery_btn delivery_temp" data-temp="0" value="常溫配送" required>
+          <input type="button" class="btn delivery_btn delivery_temp" data-temp="1" value="低溫配送" required>
         </div>
       </section>
 
-      <section>
+      <section class="form-group col-md-12 delivery_payment">
         <h3>付款方式</h3>
         <hr>
-        <div class="d-flex flex-column">
-          <label class="" for="payment2"><input type="radio" id="payment2" name="paymentRadios" value="2" required>線上刷卡</label>
-          <label class="" for="payment3"><input type="radio" id="payment3" name="paymentRadios" value="3" required>貨到付款</label>
+        <div class="d-flex center_all btn-group">
+          <input type="button" class="btn delivery_btn delivery_pay" data-temp="0" value="線上刷卡" required>
+          <input type="button" class="btn delivery_btn delivery_pay" data-temp="1" value="貨到付款" required>
         </div>
       </section>
 
-      <section>
+      <section class="form-group col-md-12 delivery_receipt">
         <h3>發票</h3>
         <hr>
         <div class="d-flex flex-column">
-          <label class="" for="receipt2"><input type="radio" id="receipt2" name="receiptRadios" value="2" required>電子發票</label>
-          <label class="" for="receipt3"><input type="radio" id="receipt3" name="receiptRadios" value="3" required>公司發票</label>
+          <div class="d-flex center_all btn-group">
+            <input type="button" class="btn delivery_btn delivery_receipt" data-rec="0" value="電子發票" required>
+            <input type="button" class="btn delivery_btn delivery_receipt" data-rec="1" value="貨到付款" required>
+          </div>
+          <input type="text" class="mt-5 p-3" placeholder="統編" value="">
         </div>
-        <input type="text" class="tax-num-cky" placeholder="統編" required>
+      </section>
+
+      <section class="form-group col-md-12">
+        <hr>
+        <div class=" d-flex justify-content-between btn-group">
+          <input type="submit" class="btn delivery_btn" value="返回上一步">
+          <input type="submit" class="btn delivery_btn" value="完成">
+        </div>
       </section>
     </form>
 
-    <div class="col-md-3">
-      <section class="shippingDuration-cky">
+    <div class="col-md-3 checkout_info">
+      <section class="checkout_info_help">
         <hgroup>
           <h5>快速到貨</h5>
-          <h6>每日下午 14:00前完成訂單，可於下一個工作天宅配到貨，4個工作天內通知指定門市取貨。</h6>
+          <p>每日下午 14:00前完成訂單，可於下一個工作天宅配到貨，4個工作天內通知指定門市取貨。</p>
         </hgroup>
       </section>
       <?php require_once('./tpl/cart/tpl-cart.php'); ?>
--- tpl/cart/tpl-cart.php
+++ tpl/cart/tpl-cart.php
@@ -1,25 +1,27 @@
-<section>
+<section class="checkout_cart">
   <h4>購物車<i class="fa fa-shopping-cart"></i></h4>
   <div class="d-flex flex-column">
-    <a href="#">ITEM1<span class="price">$</span></a>
-    <a href="#">ITEM2<span class="price">$</span></a>
-    <a href="#">ITEM3<span class="price">$</span></a>
+    <a class="d-flex flex-wrap justify-content-between checkout_cart_item">
+      <span class="w-100 checkout_item_name">八甲田 純米大吟醸 華想い40</span>
+      <span class="checkout_item_count">4</span>
+      <span class="checkout_item_price">1000</span>
+    </a>
   </div>
 </section>
 
-<section>
+<section class="checkout_price">
   <ul>
     <li class="d-flex justify-content-between">            
-      <span class="">商品總額</span>
-      <span class="">$ 4,250</span>
+      <h6>商品總額</h6>
+      <p class="checkout_price_raw">4,250</p>
     </li>
     <li class="d-flex justify-content-between">            
-      <span class="">運費</span>
-      <span class="">$ 150</span>
+      <h6>運費</h6>
+      <p class="checkout_price_ship">150</p>
     </li>
     <li class="d-flex justify-content-between">            
-      <span class="">總金額</span>
-      <span class="">$ 4,400</span>
+      <h6>總金額</h6>
+      <p class="checkout_price_total">4,400</p>
     </li>
   </ul>
 </section>
