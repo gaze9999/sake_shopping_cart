016cf62a4a28d4b6e8cfb58a2f611aaa7ef00e93
gaze9999 <gaze9999@livemail.tw>
2020-01-11 17:08:48 +0800

    change layout

--- checkout.php
+++ checkout.php
@@ -9,59 +9,60 @@ require_once('./tpl/tpl-header.php');
 </style>
 <main class="container checkout_frame">
   <div class="row">
-    <form class="col-md-9 d-flex flex-column" action="">
-      <section class="">
-        <h4>請選擇付款方式</h4>
-        <div>
-          <label class="" for="p2p">貨到付款</label>
-          <input type="radio" name="shipment" id="p2p">
-          <label class="" for="p2o">線上付款</label>
-          <input type="radio" name="shipment" id="p2o">
-        </div>
-      </section>
-      <section class="d-flex flex-column py-4">
-        <h4>請填寫信用卡資料</h4>
-        <div class="card_type">
-          <i class="fab fa-cc-visa" style="color:navy;"></i>
-          <i class="fab fa-cc-amex" style="color:blue;"></i>
-          <i class="fab fa-cc-mastercard" style="color:red;"></i>
-          <i class="fab fa-cc-discover" style="color:orange;"></i>
-        </div>
-        <div class="d-flex flex-column">
-          <label for="card_name">持卡人姓名</label>
-          <input type="text" id="card_name" name="cardname" placeholder="John Doe">
-          <label for="card_num">信用卡號</label>
-          <input type="text" id="card_num" name="cardnumber" placeholder="1111-2222-3333-4444">
-        </div>
-        <div class="d-flex flex-column py-4">
-          <h4>有效日期及安全碼</h4>
-          <div class="d-flex">
-            <div class="flex-grow-1">
-              <label for="expyear">Exp Year</label>
-              <input class="" type="text" id="expyear" name="expyear" placeholder="MM/YY">
+    <form class="form-row col-md-9" method="POST" action="">
+      <section class="form-group col-md-12">
+        <h4>付款方式</h4>
+        <hr>
+        <section class="form-row">
+          <div class="form-group d-flex col-md-12">
+            <div class="form-check mr-3">
+              <label class="form-check-label" for="p2p"><input class="form-check-input" type="radio" name="shipment" id="p2p" disabled>貨到付款</label>
             </div>
-            <div class="flex-grow-1">
-              <label for="cvv">CVV</label>
-              <input class="" type="text" id="cvv" name="cvv" placeholder="789">
+            <div class="form-check ml-3">
+              <label class="form-check-label" class="" for="p2o"><input class="form-check-input" type="radio" name="shipment" id="p2o" disabled>線上付款</label>
             </div>
           </div>
+          <div class="form-group col-md-12 card_type">
+            <i class="fab fa-cc-visa" style="color:navy;"></i>
+            <i class="fab fa-cc-amex" style="color:blue;"></i>
+            <i class="fab fa-cc-mastercard" style="color:red;"></i>
+            <i class="fab fa-cc-discover" style="color:orange;"></i>
+          </div>
+        </section>
+      </section>
+      
+      <section class="form-group col-md-6 mt-5">
+        <h4>請填寫信用卡資料</h4>
+        <hr>
+        <div class="d-flex flex-column">
+          <label for="card_name">持卡人姓名</label>
+          <input class="form-control" type="text" id="card_name" name="cardname" placeholder="John Doe" required>
+          <label for="card_num">信用卡號</label>
+          <input class="form-control" type="text" id="card_num" name="cardnumber" placeholder="1111-2222-3333-4444" required>
         </div>
       </section>
-      <section>
+
+      <section class="form-group col-md-6 mt-5">
+        <h4>有效日期及安全碼</h4>
+        <hr>
+        <div class="d-flex flex-column">
+          <label for="expyear">Exp Year</label>
+          <input class="form-control" type="text" id="expyear" name="expyear" placeholder="MM/YY" required>
+        </div>
+        <div class="d-flex flex-column">
+          <label for="cvv">CVV</label>
+          <input class="form-control" type="text" id="cvv" name="cvv" placeholder="789" required>
+        </div>
+      </section>
+
+      <section class="form-group col-md-12">
         <input class="btn" type="submit" value="返回上一步" class="p-prebtn-hyh">
         <input class="btn" type="submit" value="完成" class="p-btn-hyh">
       </section>
     </form>
 
-    <div class="col-md-3">
-      <h4>購物車<i class="fa fa-shopping-cart"></i></h4>
-      <div class="d-flex flex-column">
-        <a href="#">ITEM1<span class="price">$</span></a>
-        <a href="#">ITEM2<span class="price">$</span></a>
-        <a href="#">ITEM3<span class="price">$</span></a>
-      </div>
-      <hr>
-      <p>總金額: </p>
+    <div class="col-md-3">      
+      <?php require_once('./tpl/cart/tpl-cart.php'); ?>
     </div>
   </div>
 </main>
