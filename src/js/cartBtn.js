efbecfecb848aad5fbd50aa25a5e2d7455164f14
gaze9999 <gaze9999@livemail.tw>
2020-01-12 22:20:28 +0800

    checkout cart add function

--- src/js/cartBtn.js
+++ src/js/cartBtn.js
@@ -2,7 +2,7 @@ carGoCheckout.on('mouseup',(e) => {
   let capIds = [],
       itemQtys = []
 
-      cartHiddenQty.each(e => {
+  cartHiddenQty.each(e => {
     itemQtys.push(parseInt(cartHiddenQty[e].value))
   })
 
@@ -10,28 +10,54 @@ carGoCheckout.on('mouseup',(e) => {
     capIds.push(parseInt(cartHiddenSid[e].value))
   })
 
-  fetch('./func/func-goDelivery.php', {
-    method: "POST",
-    headers: {'Content-Type': 'application/json',
-              'Accept': 'application/json'},
-    body: [JSON.stringify({
-      'capIds': capIds,
-      'qtys': itemQtys
-    })]
-  }).then(res => {
-    if (res.status >= 200 && res.status < 300) {
-      return res.json();
-    } else {
-      let error = new Error(res.statusText);
-      error.response = response;
-      throw error.Content-Type;
-    }
-  }).then(json => {
+  updateSession()
 
-  }).catch(error => {
-    console.log('getDetails() request failed:', error);
-    console.log(error.response);
-  });
+  function updateSession() {
+    fetch('./func/func-updateSession.php', {
+      method: "POST",
+      headers: {'Content-Type': 'application/json',
+                'Accept': 'application/json'},
+      body: [JSON.stringify({
+        'capIds': capIds,
+        'qtys': itemQtys
+      })]  
+    }).then(res => {
+      if (res.status >= 200 && res.status < 300) {
+        return res.json();
+      } else {
+        let error = new Error(res.statusText);
+        error.response = response;
+        throw error.Content-Type;
+      }
+  
+    }).catch(error => {
+      console.log('getDetails() request failed:', error);
+      console.log(error.response);
+    });
+  }
+
+  // fetch('./func/func-goDelivery.php', {
+  //   method: "POST",
+  //   headers: {'Content-Type': 'application/json',
+  //             'Accept': 'application/json'},
+  //   body: [JSON.stringify({
+  //     'capIds': capIds,
+  //     'qtys': itemQtys
+  //   })]
+  // }).then(res => {
+  //   if (res.status >= 200 && res.status < 300) {
+  //     return res.json();
+  //   } else {
+  //     let error = new Error(res.statusText);
+  //     error.response = response;
+  //     throw error.Content-Type;
+  //   }
+  // }).then(json => {
+
+  // }).catch(error => {
+  //   console.log('getDetails() request failed:', error);
+  //   console.log(error.response);
+  // });
 
   location.href = './delivery.php';
 })
---
+++ src/js/variables/delivery.js
@@ -0,0 +1,9 @@
+    // checkout cart
+let ccList = $('.checkout_cart_list'),
+    ccName = $('.checkout_item_name'),
+    ccCount = $('.checkout_item_count'),
+    ccPrice = $('.checkout_item_price'),
+
+    // checkout total
+    ctRaw = $('.checkout_price_raw'),
+    ctTotal = $('.checkout_price_total')
