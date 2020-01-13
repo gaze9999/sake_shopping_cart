bfb039c0ab520feb01e5a90367e4a3e8a405ee79
gaze9999 <gaze9999@livemail.tw>
2020-01-13 23:58:39 +0800

    using owl carousel cdn

--- tpl/tpl-html-head.php
+++ tpl/tpl-html-head.php
@@ -11,9 +11,11 @@
   <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC|Noto+Serif+TC&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- --- Owl-Carousel --->
-  <link rel="stylesheet" type="text/css" href="./dist/css/owl.carousel.min.css">
-  <link rel="stylesheet" type="text/css" href="./dist/css/owl.theme.default.min.css">
-  <!-- Animate On Scroll -->
+  <!-- <link rel="stylesheet" type="text/css" href="./dist/css/owl.carousel.min.css">
+  <link rel="stylesheet" type="text/css" href="./dist/css/owl.theme.default.min.css"> -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+    <!-- Animate On Scroll -->
   <link rel="stylesheet" type="text/css" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
   <!-- custom css -->
   <link rel="stylesheet" type="text/css" href="./dist/css/all.min.css">
