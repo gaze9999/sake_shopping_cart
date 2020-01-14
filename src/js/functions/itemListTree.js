e266a87c3dca7cd34a5709f92b1fff2324956f24
gaze9999 <gaze9999@livemail.tw>
2020-01-14 16:40:51 +0800

    item tree collapse

--- src/js/functions/itemListTree.js
+++ src/js/functions/itemListTree.js
@@ -54,52 +54,45 @@ function getRTree(rId) {
     }
 
   }).then(json => {
-    treeList.append(
-      `<dt class="col-12">分類</dt>
-      <dd class="col-12">
-        <button class='btn tree_btn'>全部</button>
-      </dd>`)
-
     for (let i in json[0]) {
-      treeList.append(
-        `<dt class="col-3 col-md-12 px-1">
-          <button class='btn tree_btn tree_rid' data-rid='${json[0][i]["rId"]}'>${json[0][i]["regionName"]}</button>
+      $('#tree_wrapper1').append(
+        `<dt class="tree_selection p-0">
+          <button class='btn tree_btn tree_rid' data-rid='${json[0][i]["rId"]}'>${json[0][i]["regionName"].slice(0, json[0][i]["regionName"].length-2)}</button>
         </dt>`)
     }
-
-    treeList.append(
-      `<dt class="col-12">篩選</dt>
-      <dd class="col-12">
-        <button class='btn tree_btn'>全選</button>
-      </dd>`)
     for (let i in json[1]) {
-      treeList.append(
-        `<dt class="col-3 col-md-12 px-1">
-          <input type="checkbox" class='btn tree_btn tree_vid' name='datavid' id='datavid_${json[1][i]["vcId"]}' data-vid='${json[1][i]["vcId"]}'>
-          <label class="btn tree_btn tree_vid_label" for="datavid_${json[1][i]["vcId"]}" data-vid='${json[1][i]["vcId"]}'>${json[1][i]["vCatag"]}</label>
+      $('#tree_wrapper2').append(
+        `<dt class="tree_selection p-0">
+          <button class='btn tree_btn tree_vid' data-vid='${json[1][i]["vcId"]}'>${json[1][i]["vCatag"]}</button>
         </dt>`)
     }
-    treeList.append(
-      `<dt class="col-12">容量</dt>
-      <dd class="col-12">
-        <button class='btn tree_btn'>全選</button>
-      </dd>`)
     for (let i in json[2]) {
-      treeList.append(
-        `<dt class="col-3 col-md-12 px-1">
-          <input type="checkbox" class='btn tree_btn tree_cap' name='datacap' id='datacap_${json[2][i]["cap"]}' data-cap='${json[2][i]["cap"]}'>
-          <label class="btn tree_btn tree_cap_label" for="datacap_${json[2][i]["cap"]}"data-cap='${json[2][i]["cap"]}'>${json[2][i]["cap"]}ML</label>
+      $('#tree_wrapper3').append(
+        `<dt class="tree_selection p-0">
+          <button class='btn tree_btn tree_cap' data-cap='${json[2][i]["cap"]}'>${json[2][i]["cap"]}ML</button>
         </dt>`)
     }
     treeButton    = $('.tree_btn'),
     treeRidButton = $('.tree_rid'),
     treeVidButton = $('.tree_vid'),
-    treeCapButton = $('.tree_cap')
+    treeCapButton = $('.tree_cap'),
+    treeCapButton = $('.tree_rid_all'),
+    treeCapButton = $('.tree_vid_all'),
+    treeCapButton = $('.tree_cap_all')
 
     treeButton.on('mouseup', function() {
       let rid = $(this).data('rid')
       $(this).toggleClass('tree_checked')
+      treeCapButton.removeClass('tree_checked')
       filterCheckbox(rid)
+    })
+    
+    treeRidButton.on('mouseup', function() {
+      if (treeRidButton.hasClass('tree_checked')) {
+        treeRidButton.removeClass('tree_checked')
+      }
+        $(this).addClass('tree_checked')
+        btnSelection($(this))
     })
 
 
@@ -108,3 +101,4 @@ function getRTree(rId) {
     // return error.response.json();
   });
 }
+
